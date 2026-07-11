052479a83a8fc0c3f36b43e1adc9f4c1d0aee945
LahariMadiri <laharimadiri@gmail.com>
2026-07-11 14:57:49 +0530

    first python file

---
+++ variables.py
@@ -0,0 +1,22 @@
+# variables
+
+#strings 
+First_Name = "John"
+Food = "Pizza"
+email = "john@fake.com"
+print("First_Name" , "Food", "email")
+
+# integers
+age = 20
+quantity = 6
+no_of_items = 10
+print ("age" , "quantity", "no_of_items")
+
+# floats
+price = 10.5
+weight = 2.5
+print("price", "weight")
+
+# boolean
+is_adult = True
+print("is_adult")
