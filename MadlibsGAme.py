5f1e27f4511be3c97c83b2a35f5147592ca768ce
LahariMadiri <laharimadiri@gmail.com>
2026-07-15 09:14:58 +0530

    4th file

---
+++ MadlibsGAme.py
@@ -0,0 +1,12 @@
+# MADLIBS GAME
+
+Adjective1 = input("Enter an adjective: ")
+Noun1 = input("Enter a noun: ")
+Adjective2 = input("Enter another adjective: ")
+Verb1 = input("Enter a verb: ")
+Adjective3 = input("Enter one more adjective: ")
+
+print(f"Today i went to the {Adjective1} zoo.")
+print(f"In an exhibit, I saw a {Noun1}.")
+print(f"{Noun1} was very {Adjective2} and {Verb1}.")
+print(f"Overall, it was a {Adjective3} experience!")
